c4a1435993adf38fe4c7b9caf27b520d58f798a9
gllucass <49792972+gllucass@users.noreply.github.com>
2023-07-02 06:28:04 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Maxwellen, eu te amo. Se você me ama dá uma risadinha.</h1>
+    return <h1>Maxwellen, eu te amo. Se você me ama, dá uma risadinha! </h1>
 }
 
 export default Home;
